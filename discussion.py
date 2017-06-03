6fb929445dedcd615e5a8d66441c50db3d91a82d
vagrant <vagrant@vagrant.vm>
2017-06-03 17:30:22 +0000

    first commit

---
+++ discussion.py
@@ -0,0 +1,21 @@
+"""
+Part 1: Discussion Questions
+
+<<< Recursion >>>
+
+1. Recursion is when a function calls itself to perform iterable actions.
+
+2. A base case tells the recursion when to stop. Without a base case, the
+recursion would happen forever and you would never finish what you are trying
+to do.
+
+<<< Graphs >>>
+
+1. A graph is a tree with loops ('cyclic').
+
+2. Graphs don't contain a root and do not represent a hierarchy.
+
+3. A city's public transit routes would be good to model with a graph - it
+would be a cyclic, un-directed graph.
+
+"""
